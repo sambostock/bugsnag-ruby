bec8b6c1b5b2dc0781faa59ba68b68465eae34ae
Sam Bostock <sam.bostock@shopify.com>
2022-03-02 20:19:09 -0500

    [WIP] Tweaks

--- lib/bugsnag/integrations/graphql.rb
+++ lib/bugsnag/integrations/graphql.rb
@@ -7,21 +7,23 @@ module Bugsnag
         def before_query(query)
           Bugsnag.configuration.set_request_data(:graphql, {
             # NOTE: Dumping in a bunch of stuff from https://graphql-ruby.org/api-doc/2.0.2/GraphQL/Query#operation_name-instance_method until we know what's useful
-            operation_name: query.operation_name,
             schema: {
               name: query.schema.name,
               description: query.schema.description,
             },
+
+            operation: {
+              name: query.selected_operation_name,
+              type: query.selected_operation&.operation_type, # Do we need &. here?
+            },
+
             sanitized_query_string: query.sanitized_query_string,
 
+            # Is there value in these?
             fingerprint: query.fingerprint,
             operation_fingerprint: query.operation_fingerprint,
             variables_fingerprint: query.variables_fingerprint,
 
-            is_mutation: query.mutation?,
-            is_query: query.query?,
-            is_subscription: query.subscription?,
-            is_valid: query.valid?,
             # ...
           })
         end
@@ -30,15 +32,15 @@ module Bugsnag
           # TODO: The error is probably rescued and reported
           # from somewhere outside the query, so we probably
           # want to leave the report_data alone? TBD...
+          # Then again, some reference apps clear context here...
         end
       end
     end
   end
 end
 
-each_graphql_schema do |schema| # TODO: Figure out if this makes sense...
-  schema.instrument(:query, Bugsnag::GraphQL::Instrumentation)
-  schema.instrument(:mutation, Bugsnag::GraphQL::Instrumentation)
-end
+GraphQL::Schema.instrument(:query, Bugsnag::GraphQL::Instrumentation)
+GraphQL::Schema.instrument(:mutation, Bugsnag::GraphQL::Instrumentation)
+GraphQL::Schema.instrument(:subscription, Bugsnag::GraphQL::Instrumentation)
 
 Bugsnag.configuration.internal_middleware.use(Bugsnag::Middleware::GraphQL)
