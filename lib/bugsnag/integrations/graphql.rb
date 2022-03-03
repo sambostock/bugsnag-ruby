4b9fb51c7a20fbe46e464117b188e54276531bd7
Sam Bostock <sam.bostock@shopify.com>
2022-03-02 21:54:29 -0500

    [WIP] Save query and lazy generate payload

--- lib/bugsnag/integrations/graphql.rb
+++ lib/bugsnag/integrations/graphql.rb
@@ -5,29 +5,7 @@ module Bugsnag
     module Instrumentation
       class << self
         def before_query(query)
-          # TODO: Should we simply save the query here, and lazy compute all of this instead?
-
-          Bugsnag.configuration.set_request_data(:graphql, {
-            # NOTE: Dumping in a bunch of stuff from https://graphql-ruby.org/api-doc/2.0.2/GraphQL/Query#operation_name-instance_method until we know what's useful
-            schema: {
-              name: query.schema.name,
-              description: query.schema.description,
-            },
-
-            operation: {
-              name: query.selected_operation_name,
-              type: query.selected_operation&.operation_type, # Do we need &. here?
-            },
-
-            sanitized_query_string: query.sanitized_query_string,
-
-            # Is there value in these?
-            fingerprint: query.fingerprint,
-            operation_fingerprint: query.operation_fingerprint,
-            variables_fingerprint: query.variables_fingerprint,
-
-            # ...
-          })
+          Bugsnag.configuration.set_request_data(:graphql_query, query)
         end
 
         def after_query(query)
@@ -35,6 +13,8 @@ module Bugsnag
           # from somewhere outside the query, so we probably
           # want to leave the report_data alone? TBD...
           # Then again, some reference apps clear context here...
+
+          # Bugsnag.configuration.unset_request_data(:graphql_query, query)
         end
       end
     end
--- lib/bugsnag/middleware/graphql.rb
+++ lib/bugsnag/middleware/graphql.rb
@@ -8,14 +8,39 @@ module Bugsnag
       end
 
       def call(report)
-        graphql = report.request_data[:graphql]
-
-        if graphql
-          report.add_tab(:graphql, graphql)
-          # TODO: Should we be setting automatic_context?
-        end
+        query = report.request_data[:graphql_query]
+        report.add_tab(:graphql, tab_data_for(query)) if query
 
         @bugsnag.call(report)
+      end
+
+      private
+
+      def tab_data_for(query)
+        {
+          # NOTE: Dumping in a bunch of stuff from https://graphql-ruby.org/api-doc/2.0.2/GraphQL/Query#operation_name-instance_method until we know what's useful
+
+          schema: {
+            name: query.schema.name,
+            description: query.schema.description,
+          },
+
+          operation: {
+            name: query.selected_operation_name,
+            type: query.selected_operation&.operation_type, # Do we need &. here?
+          },
+
+          sanitized_query_string: query.sanitized_query_string,
+
+          # Is there value in these?
+          fingerprints: {
+            query: query.fingerprint,
+            operation: query.operation_fingerprint,
+            variables: query.variables_fingerprint,
+          }
+
+          # ...
+        }
       end
     end
   end
